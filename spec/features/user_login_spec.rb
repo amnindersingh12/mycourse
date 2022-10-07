258c51b3f3a18a71965602ffa8fd28c3b247e3ec
amninder singh <45424062+amnindersingh12@users.noreply.github.com>
2022-10-07 17:22:59 +0530

    Add User Login feature spec

---
+++ spec/features/user_login_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.feature 'User', type: :feature do
+  given(:present_user) { create(:member_user) }
+
+  feature 'Successful Login' do
+    scenario 'Signing in with correct credentials' do
+      visit '/auth/login'
+      within('#new_user') do
+        fill_in 'Email', with: present_user.email
+        fill_in 'Password', with: present_user.password
+      end
+      click_button 'commit'
+      expect(page).to have_content 'Signed in successfully'
+    end
+  end
+
+  feature 'Unsuccessful Login' do
+    scenario 'Signing in with incorrect email' do
+      visit '/auth/login'
+      within('#new_user') do
+        fill_in 'Email', with: 'first@gmail.com'
+        fill_in 'Password', with: present_user.password
+      end
+      click_button 'commit'
+      expect(page).to have_content 'Invalid Email or password'
+    end
+    scenario 'Signing in with incorrect password' do
+      visit '/auth/login'
+      within('#new_user') do
+        fill_in 'Email', with: present_user.email
+        fill_in 'Password', with: '11111111'
+      end
+      click_button 'commit'
+      expect(page).to have_content 'Invalid Email or password'
+    end
+  end
+end
